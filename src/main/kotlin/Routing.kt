d55e443b464d7ddae05919919cd09df879bf9ec2
Dymytryi <fighttournament2@gmail.com>
2025-03-09 15:35:12 +0200

    Initial commit

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,16 @@
+package com.bugbender
+
+import io.ktor.serialization.gson.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
